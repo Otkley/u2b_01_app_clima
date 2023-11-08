68bfc7b9fc9d9cf89f81c9bf6b2081d6af0e71f9
Sergio Montiel <sergiomontielcastello@gmail.com>
2023-11-07 22:35:16 -0600

    configuración del backend

--- back_end/server.rb
+++ back_end/server.rb
@@ -1,7 +1,30 @@
 require "bundler"; Bundler.require
 require "sinatra"
+require "net/http"
+require "json"
 
 get "/weather" do
   # la respuesta que daremos sera en formato json
-  content_type :json 
+  content_type :json
+  base_url = "http://api.weatherstack.com"
+  api_key = "ae2341d64533339f78b4574f533c3555"
+  method = "/current"
+  location = params[:location]
+
+  url = "#{base_url}#{method}?access_key#{api_key}&query=#{location}"
+
+  url = URI.parse(url) # Instancia de la ruta
+  http = Net::HTTP.new(url.host, url.port) # Instancia el cliente http
+  response = http.get(url.request_uri) # Instancia de la respuesta
+
+  body = JSON.parse(response.body)
+  puts "-> Codigo: #{response.code}"
+  puts "-> Cuerpo: #{body}"
+
+  if response.code != "200"
+    status 500
+    { error: "Error al obtener el clima" }
+  else
+    body.to_json
+  end
 end
